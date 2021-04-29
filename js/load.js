6bddcc9171e8a8963be878f835ef334c22aeb63f
Rogério Portela da Luz <sis-rogeriodaluz@uniguacu.edu.br>
2021-04-29 17:09:39 -0300

    Create load.js

---
+++ js/load.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    
+    $("main").load("html/principal.html");
+    
+    // Executa o evento CLICK em todos os links do menu
+    $("#carregar").click(function(e){
+        e.preventDefault();
+        
+        // Faz o carregamento da página de acordo com o COD da página, que vai pegar os valores da página.
+        $("main").load($(this).attr("href"));
+    });
+
+});
+
+
+function carregar(){
+    //Fecha modal
+    $(".modal").modal("toggle");
+
+    // Faz o carregamento da página de acordo com o COD da página, que vai pegar os valores da página.
+    $("main").load("html/cadastro.html");
+}
